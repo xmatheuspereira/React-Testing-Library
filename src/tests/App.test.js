f3a9c6d9850a8c2bc6ec9a41f23feafaa518c24f
Matheus Pereira <matheusdimmy@hotmail.com>
2022-02-28 16:03:11 -0300

    Requisito 1 OK

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+  () => {
+    test('O primeiro link deve possuir o texto "Home"', () => {
+      renderWithRouter(<App />);
+      const homeTitle = screen.getByRole('link', {
+        name: 'Home',
+      });
+      expect(homeTitle).toBeInTheDocument();
+    });
+
+    test('O segundo link deve possuir o texto "About"', () => {
+      renderWithRouter(<App />);
+      const aboutTitle = screen.getByRole('link', {
+        name: 'About',
+      });
+      expect(aboutTitle).toBeInTheDocument();
+    });
+
+    test('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+      renderWithRouter(<App />);
+      const favoritesTitle = screen.getByRole('link', {
+        name: 'Favorite Pokémons',
+      });
+      expect(favoritesTitle).toBeInTheDocument();
+    });
+  });
